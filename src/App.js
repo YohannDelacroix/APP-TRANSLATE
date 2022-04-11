7316f330b2bad493e9d73797554fe0aed23e5131
Yohann <yohann.delacroix@hotmail.fr>
2022-04-11 23:11:18 +0200

    Code source

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header/Header.js'
+import Footer from './Components/Footer/Footer.js'
+import './Cover.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="d-flex h-100 text-center text-white bg-dark">
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <Header />
+        <main className="px-3">
+          <h1>Cover your page.</h1>
+          <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+          <p className="lead">
+            <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+          </p>
+        </main>
+
+        <Footer />
+      </div>
+    </body>
+
   );
 }
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../Cover.css'
+
+function Footer(){
+  return (
+    <footer class="mt-auto text-white-50">
+      <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+    </footer>
+  );
+}
+
+export default Footer;
